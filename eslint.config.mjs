3d5473daa217d85feade4e05a6340c2592c8f04e
“sidra <fsidra167@gmail.com>
2025-01-03 12:42:42 +0500

    commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,3 @@
-// import { dirname } from "path";
-// import { fileURLToPath } from "url";
-// import { FlatCompat } from "@eslint/eslintrc";
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-// const compat = new FlatCompat({
-//   baseDirectory: __dirname,
-// });
-
-// const eslintConfig = [
-//   ...compat.extends("next/core-web-vitals", "next/typescript"),
-// ];
-
-// export default eslintConfig;
 import { FlatCompat } from '@eslint/eslintrc'
 const compat = new FlatCompat({
   // import.meta.dirname is available after Node.js v20.11.0
@@ -23,5 +7,10 @@ const eslintConfig = [
   ...compat.config({
     extends: ['next/core-web-vitals', 'next/typescript'],
   }),
+  {
+    "rules": {
+      "@typescript-eslint/no-explicit-any": "off"
+    }
+  }
 ]
 export default eslintConfig
